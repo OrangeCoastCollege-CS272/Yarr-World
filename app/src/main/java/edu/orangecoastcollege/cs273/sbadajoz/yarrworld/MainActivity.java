50c8bac596b175ca40473917aa4c26f777664cd3
Badajoz <sbadajoz@student.cccd.edu>
2017-08-31 16:52:49 -0700

    Complete objective

--- app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sbadajoz/yarrworld/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.orangecoastcollege.cs273.sbadajoz.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchToast(View view) {
+        Toast.makeText(this, getString(R.string.shiver_toast_text), Toast.LENGTH_SHORT).show();
+
     }
 }
